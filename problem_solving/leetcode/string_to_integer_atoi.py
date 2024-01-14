adf5ed1cf702af55dc556539a8bee18a462fa93f
amalviya <ashutoshmalviya7@gmail.com>
2024-01-14 19:31:22 +0530

    leetcode problem and solutions

---
+++ problem_solving/leetcode/string_to_integer_atoi.py
@@ -0,0 +1,117 @@
+"""
+
+URL: https://leetcode.com/problems/string-to-integer-atoi/description/
+
+Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer (similar to C/C++'s atoi function).
+
+The algorithm for myAtoi(string s) is as follows:
+
+Read in and ignore any leading whitespace.
+Check if the next character (if not already at the end of the string) is '-' or '+'. Read this character in if it is either. This determines if the final result is negative or positive respectively. Assume the result is positive if neither is present.
+Read in next the characters until the next non-digit character or the end of the input is reached. The rest of the string is ignored.
+Convert these digits into an integer (i.e. "123" -> 123, "0032" -> 32). If no digits were read, then the integer is 0. Change the sign as necessary (from step 2).
+If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then clamp the integer so that it remains in the range. Specifically, integers less than -231 should be clamped to -231, and integers greater than 231 - 1 should be clamped to 231 - 1.
+Return the integer as the final result.
+Note:
+
+Only the space character ' ' is considered a whitespace character.
+Do not ignore any characters other than the leading whitespace or the rest of the string after the digits.
+
+
+Example 1:
+
+Input: s = "42"
+Output: 42
+Explanation: The underlined characters are what is read in, the caret is the current reader position.
+Step 1: "42" (no characters read because there is no leading whitespace)
+         ^
+Step 2: "42" (no characters read because there is neither a '-' nor '+')
+         ^
+Step 3: "42" ("42" is read in)
+           ^
+The parsed integer is 42.
+Since 42 is in the range [-231, 231 - 1], the final result is 42.
+Example 2:
+
+Input: s = "   -42"
+Output: -42
+Explanation:
+Step 1: "   -42" (leading whitespace is read and ignored)
+            ^
+Step 2: "   -42" ('-' is read, so the result should be negative)
+             ^
+Step 3: "   -42" ("42" is read in)
+               ^
+The parsed integer is -42.
+Since -42 is in the range [-231, 231 - 1], the final result is -42.
+Example 3:
+
+Input: s = "4193 with words"
+Output: 4193
+Explanation:
+Step 1: "4193 with words" (no characters read because there is no leading whitespace)
+         ^
+Step 2: "4193 with words" (no characters read because there is neither a '-' nor '+')
+         ^
+Step 3: "4193 with words" ("4193" is read in; reading stops because the next character is a non-digit)
+             ^
+The parsed integer is 4193.
+Since 4193 is in the range [-231, 231 - 1], the final result is 4193.
+"""
+
+
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        is_alpha = False
+        sign = []
+        digits = []
+        digit_dict = {
+            '0': 0,
+            '1': 1,
+            '2': 2,
+            '3': 3,
+            '4': 4,
+            '5': 5,
+            '6': 6,
+            '7': 7,
+            '8': 8,
+            '9': 9,
+        }
+
+        for i in s:
+            if i != ' ':
+                if (i == '-' or i == '+') and len(digits) == 0:
+                    sign.append(i)
+                elif (i == '-' or i == '+') and len(digits) != 0:
+                    break
+
+            if (len(digits) != 0 or len(sign) != 0) and i == ' ':
+                break
+
+            if i in digit_dict.keys():
+                digits.append(digit_dict[i])
+            elif i not in digit_dict.keys() and i != ' ' and i != '-' and i != '+':
+                break
+
+        num = 0
+        length = len(digits)
+
+        if not length:
+            return 0
+
+        if len(sign) > 1:
+            return 0
+
+        for k in digits:
+            length -= 1
+            num = num + (k * (10 ** length))
+
+        if num > 2 ** 31 - 1:
+            if len(sign) == 1 and sign[0] == '-':
+                return -2 ** 31
+            return 2 ** 31 - 1
+
+        if len(sign) == 1 and sign[0] == '-':
+            return num * -1
+
+        return num
