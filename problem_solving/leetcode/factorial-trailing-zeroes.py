b6efda72727dcae435877c6a99ddecb333aadcb7
Ashutosh Malviya <ashutoshmalviya7@gmail.com>
2026-04-23 12:07:15 +0530

    Add factorial trailing zeroes solution
    Implement a solution to count trailing zeroes in factorial.

---
+++ problem_solving/leetcode/factorial-trailing-zeroes.py
@@ -0,0 +1,30 @@
+"""Link: https://leetcode.com/problems/factorial-trailing-zeroes"""
+
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        sys.set_int_max_str_digits(100000)
+        if n in [0,1,2,3,4]:
+            return 0
+
+        dp = [0]*2
+        dp[0] = 1
+        dp[1] = 2
+
+        i = 3
+        while i <= n:
+            dp[1] = dp[1] * i
+            i += 1
+        
+        fac = str(dp[-1])
+
+        i = len(fac)-1
+        count = 0
+        while i >= 0:
+            if fac[i] != "0":
+                return count
+            else:
+                count += 1
+            
+            i -= 1
+
+        return count
