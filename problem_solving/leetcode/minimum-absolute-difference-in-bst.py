c23d70f4b82ec329d786b8bdc49b6e597e26fd1e
Ashutosh Malviya <ashutoshmalviya7@gmail.com>
2026-04-24 13:18:44 +0530

    Implement DFS approach for minimum absolute difference in BST
    Added a new approach using DFS to find minimum absolute difference in BST.

--- problem_solving/leetcode/minimum-absolute-difference-in-bst.py
+++ problem_solving/leetcode/minimum-absolute-difference-in-bst.py
@@ -31,3 +31,33 @@ class Solution:
                 min_diff = min(min_diff, abs(all_nodes[i] - all_nodes[j]))
             
         return min_diff
+
+# Approach 2:
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+        
+        self.prev = None
+        self.min_diff = float("+inf")
+        def dfs(node):
+            if not node:
+                return
+
+            dfs(node.left)
+
+            if self.prev is not None:
+                self.min_diff = min(self.min_diff, abs(self.prev - node.val))
+
+            self.prev = node.val
+        
+            dfs(node.right)
+
+        dfs(root)
+            
+        return self.min_diff
