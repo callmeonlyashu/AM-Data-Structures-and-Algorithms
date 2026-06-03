20bb0726d447fc704892760b4e4f66735a66449f
Ashutosh Malviya <ashutoshmalviya7@gmail.com>
2026-06-03 12:41:45 +0530

    Optimize tree construction using a dictionary for indices

--- problem_solving/leetcode/construct-binary-tree-from-inorder-and-postorder-traversal.py
+++ problem_solving/leetcode/construct-binary-tree-from-inorder-and-postorder-traversal.py
@@ -9,12 +9,14 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
-      
+
 class Solution:
     def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
         tree = TreeNode()
         total_nodes = len(postorder)
         tree.val = postorder[-1]
+
+        inorder_dict = {num:idx for idx, num in enumerate(inorder)}
 
         for i in range(total_nodes-2, -1, -1):
             node = tree
@@ -22,8 +24,8 @@ class Solution:
                 curr_val = postorder[i]
                 # Check in in-order if it should be on the right or left of node
                 curr_root = node.val
-                idx_curr_root = inorder.index(curr_root)
-                idx_curr_val = inorder.index(curr_val)
+                idx_curr_root = inorder_dict[curr_root]
+                idx_curr_val = inorder_dict[curr_val]
 
                 # If current index is greater, then go right else left
                 if idx_curr_val > idx_curr_root:
@@ -40,6 +42,9 @@ class Solution:
                         node = node.left
 
         return tree
+
+
+        
 
 
         
