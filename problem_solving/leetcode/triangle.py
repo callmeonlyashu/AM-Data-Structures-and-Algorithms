244372be8afbe06a19307dba9c12e87dc8dfbaae
Ashutosh Malviya <ashutoshmalviya7@gmail.com>
2026-06-24 15:27:56 +0530

    Add working DP solution for triangle problem
    Implemented a working dynamic programming approach to find the minimum path sum in a triangle.

--- problem_solving/leetcode/triangle.py
+++ problem_solving/leetcode/triangle.py
@@ -2,6 +2,29 @@
 
 from typing import List
 
+# Working DP Aproach
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        n = len(triangle)
+        dp = [[] for _ in range(n)]
+        dp[0] = triangle[0]
+
+        for i in range(1,n):
+            for j in range(len(triangle[i])):
+                # left end
+                if j == 0:
+                    dp[i].append(dp[i-1][j] + triangle[i][j])
+                
+                # For the numbers in between
+                elif 0 < j < len(triangle[i])-1:
+                    dp[i].append(min(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j])
+                
+                # Right end
+                elif j == len(triangle[i])-1:
+                    dp[i].append(dp[i-1][j-1] + triangle[i][j])
+        
+        return min(dp[-1])
+
 # Failed DP Approach
 
 class Solution:
