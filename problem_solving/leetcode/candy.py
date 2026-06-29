a0e1c4b4d45d95adb894fe0b2eb87a347aa322ff
Ashutosh Malviya <ashutoshmalviya7@gmail.com>
2026-06-30 02:42:35 +0530

    Optimize candy distribution solution to O(n)
    Refactor candy function to improve efficiency from O(n^2) to O(n) by checking left and right neighbors for candy distribution.

--- problem_solving/leetcode/candy.py
+++ problem_solving/leetcode/candy.py
@@ -1,7 +1,29 @@
 """Link: https://leetcode.com/problems/candy/"""
 
-# Solution with TLE
+# O(n) Solution
 from typing import List
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        n = len(ratings)
+        candies = [1]*n
+
+        for i in range(n):
+            #  check left
+            if i-1 >= 0:
+                if ratings[i-1] < ratings[i]:
+                    candies[i] = candies[i-1] + 1
+
+        for i in range(n-1, -1, -1):
+            #  Check right
+            if i+1 < n:
+                if ratings[i+1] < ratings[i]:
+                    candies[i] = max(candies[i], candies[i+1] + 1)
+
+
+        return sum(candies)
+
+
+# Solution with TLE O(n2)
 class Solution:
     def candy(self, ratings: List[int]) -> int:
         n = len(ratings)
