0bf26d7831982e5a5342bee5c257fdf6970e48af
Ashutosh Malviya <ashutoshmalviya7@gmail.com>
2026-07-15 14:02:25 +0530

    Add stack-based BSTIterator implementation
    Implemented a stack-based approach for BST iteration using inorder traversal. Added methods next() and hasNext() to retrieve elements in sorted order.

--- problem_solving/leetcode/binary-search-tree-iterator.py
+++ problem_solving/leetcode/binary-search-tree-iterator.py
@@ -1,5 +1,44 @@
 """Link: https://leetcode.com/problems/binary-search-tree-iterator/"""
 
+# Stack based approach O(h)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class BSTIterator:
+
+    def __init__(self, root: Optional[TreeNode]):
+        self.inorder = deque([])
+        def inorder(root):
+            if not root:
+                return root
+            
+            inorder(root.left)
+            self.inorder.append(root.val)
+            inorder(root.right)
+
+        inorder(root)
+            
+    def next(self) -> int:
+        curr_element = self.inorder.popleft()
+        return curr_element 
+
+    def hasNext(self) -> bool:
+        if len(self.inorder):
+            return True
+        return False
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
+
+
+# List based approach O(n)
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
