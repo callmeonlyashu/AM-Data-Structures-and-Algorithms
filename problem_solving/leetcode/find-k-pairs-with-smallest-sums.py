e4601b61c8a918a775961be3c73dfb8cd2fd5041
Ashutosh Malviya <ashutoshmalviya7@gmail.com>
2026-07-29 12:53:16 +0530

    Add working solution for finding k smallest pairs
    Implemented a working solution using a priority queue to find k pairs with the smallest sums from two lists.

--- problem_solving/leetcode/find-k-pairs-with-smallest-sums.py
+++ problem_solving/leetcode/find-k-pairs-with-smallest-sums.py
@@ -1,5 +1,23 @@
 """Link: https://leetcode.com/problems/find-k-pairs-with-smallest-sums/"""
 
+# Working Solution
+queue = []
+def push(i, j):
+    if i < len(nums1) and j < len(nums2):
+        heapq.heappush(queue, [nums1[i] + nums2[j], i, j])
+
+push(0, 0)
+pairs = []
+while queue and len(pairs) < k:
+    _, i, j = heapq.heappop(queue)
+    pairs.append([nums1[i], nums2[j]])
+    push(i, j + 1)
+    if j == 0:
+        push(i + 1, 0)
+        
+return pairs
+
+
 # Solution with TLE
 from typing import List
 
