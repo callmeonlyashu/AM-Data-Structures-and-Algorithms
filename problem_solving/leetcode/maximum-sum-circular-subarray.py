b2a6344023780c652803e37ea186ab2bd5762312
Ashutosh Malviya <ashutoshmalviya7@gmail.com>
2026-07-31 13:28:19 +0530

    Add Kadane's algorithm for maximum circular subarray sum
    Implemented Kadane's algorithm to find the maximum sum of a circular subarray. Added handling for cases with all negative numbers and included total sum calculation.

--- problem_solving/leetcode/maximum-sum-circular-subarray.py
+++ problem_solving/leetcode/maximum-sum-circular-subarray.py
@@ -1,5 +1,36 @@
 """Link: https://leetcode.com/problems/maximum-sum-circular-subarray/"""
 
+# Kadane's Algorithm
+class Solution:
+    def maxSubarraySumCircular(self, nums: List[int]) -> int:
+        curr_max = nums[0]
+        max_sum = nums[0]
+
+        curr_min = nums[0]
+        min_sum = nums[0]
+
+        n = len(nums)
+        total = nums[0]
+
+        for i in range(1, n):
+            total += nums[i]
+            # Get curr max and max_sum so far
+            curr_max = max(curr_max + nums[i], nums[i])
+            max_sum = max(max_sum, curr_max)
+
+            # Get curr min and min_sum so far
+            curr_min = min(curr_min + nums[i], nums[i])
+            min_sum = min(min_sum, curr_min)
+        
+        # max_sum is 0 means no positive numbers are there in list
+        if max_sum < 0:
+            return max_sum
+
+        # For circular sub_array sum(nums)-min_sum is needed to check if sub_array is round the corner        
+        return max(max_sum, total-min_sum)
+
+
+# Other Approach 
 from typing import List
 class Solution:
     def maxSubarraySumCircular(self, nums: List[int]) -> int:
