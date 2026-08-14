5f00d57e5cc478be637d5c3b28b1032531b3acc7
Ashutosh Malviya <ashutoshmalviya7@gmail.com>
2026-08-14 13:42:14 +0530

    Add space optimized DFS solution for N-Queens II
    Implemented a space optimized approach for solving the N-Queens II problem using depth-first search (DFS). Added logic to track columns and diagonals to avoid conflicts.

--- problem_solving/leetcode/n-queens-ii.py
+++ problem_solving/leetcode/n-queens-ii.py
@@ -1,5 +1,40 @@
 """Link: https://leetcode.com/problems/n-queens-ii/?envType=study-plan-v2&envId=top-interview-150"""
 
+# Space Optimized Approach
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+
+        col = set()
+        posDiag = set()
+        negDiag = set()
+
+        res = 0
+        def dfs(r):
+            nonlocal res
+            if r == n:
+                res += 1
+                return res
+
+            for c in range(n):
+                if c in col or (r+c) in posDiag or (r-c) in negDiag:
+                    continue
+
+                col.add(c)
+                posDiag.add(r+c)
+                negDiag.add(r-c)
+
+                dfs(r+1)
+                
+                col.remove(c)
+                posDiag.remove(r+c)
+                negDiag.remove(r-c)
+
+        dfs(0)
+        return res
+            
+
+
+# Brute Force Backtracking Approach
 class Solution:
     def totalNQueens(self, n: int) -> int:
         board = [["."]*n for _ in range(n)]
